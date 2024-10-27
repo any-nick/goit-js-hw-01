c9eb5f2383e7cdd1c755367415c44837b151e246
Andrii Vynar <andrii.vynar.om@gmail.com>
2024-10-27 12:48:55 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,4 @@
+function getElementWidth(content, padding, border) {
+    const totalPrice = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2 ;
+    return totalPrice + "px";
+}
